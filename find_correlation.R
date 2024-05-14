d07748ff0f551a4479727befbb051664ea264d83
Mireia Andueza <mireia.andueza@estudiante.uam.es>
2024-05-14 19:49:21 +0200

    Update find_correlation.R

--- find_correlation.R
+++ find_correlation.R
@@ -1,54 +1,69 @@
 find_correlation <- function(ST , feature1, feature2, out_dir){
-
-# CAUTION: LOG NORMALIZED SEURAT OBJECT
-setwd(out_dir)
-
-# Subset of spots with gene expression > 0
-filtered <- eval(parse(text = paste0(
-  "subset(x = ST, subset = ", feature1,"> 0)")))
-filtered <- eval(parse(text = paste0(
-  "subset(x = filtered, subset = ", feature2,"> 0)")))
-
-# Split by genotype
-splitted <- SplitObject(filtered, split.by ="Genotype")
-
-# Function to compute correlation and pvalue
-cor_test_func <- function(data) {
-	DefaultAssay(data)<- "Spatial"
-	exp_feature1 <- FetchData(data, vars = feature1)[, 1]
-	exp_feature2 <- FetchData(data, vars = feature2)[, 1]
-  	cor_result <- cor.test(exp_feature1, exp_feature2)
-  	return(c(Pearson_Correlation = cor_result$estimate, 
-  	         P_Value = cor_result$p.value))
-}
-
-# Apply function to each group/genotype
-results <- sapply(splitted, cor_test_func)
-
-exp1 <- FetchData(filtered, vars = feature1)[, 1]
-exp2 <- FetchData(filtered, vars = feature2)[, 1]
-total_cor_result <- cor.test(exp1, exp2)
+	# Input:
+		# ST: Spatial transcritpmics seurat object
+		# feature1: feature in x-axis
+		# feature2: feature in y-axis
+		# out_dir: output directory
+	# Output: correlation plot
 
 
-# SCATTERPLOT
-pdf(paste0(feature1,"_", feature2, ".pdf"))
-plot2 <- FeatureScatter(filtered, feature1 = feature1, feature = feature2, 
-                        group.by = "Genotype",  cols = c("darkorchid4", 
-                                                         "mediumorchid1",
-                                                         "grey")) +
-  xlab(feature1) + 
-  ylab(feature2) + 
-  ggtitle(paste0("WT:", round(results[1,2],2),"  DD:", round(results[1,1],2),
-                 "  HET:",round(results[1,3],2)," Total:", 
-                 round(total_cor_result$estimate,2))) 
-print(plot2)
-dev.off()
+	# CAUTION: LOG NORMALIZED SEURAT OBJECT
+	setwd(out_dir)
+	
+	# Subset of spots with gene expression > 0
+	filtered <- eval(parse(text = paste0(
+	  "subset(x = ST, subset = ", feature1,"> 0)")))
+	filtered <- eval(parse(text = paste0(
+	  "subset(x = filtered, subset = ", feature2,"> 0)")))
+	
+	# Split by genotype
+	splitted <- SplitObject(filtered, split.by ="Genotype")
+	
+	# Function to compute correlation and pvalue
+	cor_test_func <- function(data) {
+		DefaultAssay(data)<- "Spatial"
+		exp_feature1 <- FetchData(data, vars = feature1)[, 1]
+		exp_feature2 <- FetchData(data, vars = feature2)[, 1]
+	  	cor_result <- cor.test(exp_feature1, exp_feature2)
+	  	return(c(Pearson_Correlation = cor_result$estimate, 
+	  	         P_Value = cor_result$p.value))
+	}
+	
+	# Apply function to each group/genotype
+	results <- sapply(splitted, cor_test_func)
+	
+	exp1 <- FetchData(filtered, vars = feature1)[, 1]
+	exp2 <- FetchData(filtered, vars = feature2)[, 1]
+	total_cor_result <- cor.test(exp1, exp2)
+	
+	
+	# SCATTERPLOT
+	pdf(paste0(feature1,"_", feature2, ".pdf"))
+	plot2 <- FeatureScatter(filtered, feature1 = feature1, feature = feature2, 
+	                        group.by = "Genotype",  cols = c("darkorchid4", 
+	                                                         "mediumorchid1",
+	                                                         "grey")) +
+	  xlab(feature1) + 
+	  ylab(feature2) + 
+	  ggtitle(paste0("WT:", round(results[1,2],2),"  DD:", round(results[1,1],2),
+	                 "  HET:",round(results[1,3],2)," Total:", 
+	                 round(total_cor_result$estimate,2))) 
+	print(plot2)
+	dev.off()
 }
 
 
 # Function for plotting feature spatially in ALL slides for 2 genotypes
 spatial_features <- function(ST, slide, feature1,feature2, out_dir){
-  
+
+	# Input:
+		# ST: Spatial transcritpmics seurat object
+		# slide: slide to plot
+		# feature1: feature in x-axis
+		# feature2: feature in y-axis
+		# out_dir: output directory
+	# Output: both features ploted on top of the slide, side by side
+	
 	library(scales)
 	setwd(out_dir)
 	exp_feature1 <- FetchData(ST, vars = feature1, layer = "data")[, 1]
@@ -77,7 +92,7 @@ spatial_features <- function(ST, slide, feature1,feature2, out_dir){
 	                       values = rescale(c(scale_range2[1],
 	                                          (scale_range2[1]+scale_range2[2])/2,
 	                                          scale_range2[2])))
- # Combine two features  
+ 	# Combine two features  
 	combined_plots <- plot1 + plot2
 	print(combined_plots)
 	dev.off()
